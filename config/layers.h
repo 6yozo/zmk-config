dddd1b7d947282f1c545dd9578ef654c72d730e5
6yozo <66yozo@gmail.com>
2023-09-06 11:22:09 +0200

    Add support for Hungarian host keyboard layout

--- config/layers.h
+++ config/layers.h
@@ -29,7 +29,21 @@
 #define WEN_DL    7
 #define WEN_TAIPO 8
 
-#define WHU_BASE  0
-#define MHU_BASE  0
-
 #define WEN_LAYER_SET WEN_BASE WEN_NAV WEN_NUM WEN_SYM WEN_FUNC WEN_AL WEN_DAL WEN_DL
+
+#define WHU_BASE  9
+#define WHU_NAV   10
+#define WHU_NUM   11
+#define WHU_SYM   12
+#define WHU_FUNC  13
+// Acute
+#define WHU_AL    14
+// Double acute
+#define WHU_DAL   15
+// Diaresis
+#define WHU_DL    16
+#define WHU_TAIPO 17
+
+#define WHU_LAYER_SET WHU_BASE WHU_NAV WHU_NUM WHU_SYM WHU_FUNC WHU_AL WHU_DAL WHU_DL
+
+#define MHU_BASE  0
