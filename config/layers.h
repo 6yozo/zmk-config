141058c738e985607e7b30e1ec0c512a2b383f4b
6yozo <66yozo@gmail.com>
2023-09-28 22:27:17 +0200

    Add bluetooth clear

--- config/layers.h
+++ config/layers.h
@@ -15,12 +15,12 @@
 
 // LAYERS FOR WIN-ENGLISH-QUERTY HOST (WEN)
 //
+#define SYSTEM    0
 // Colemak-DOX
-#define WEN_BASE  0
-#define WEN_NAV   1
-#define WEN_NUM   2
-#define WEN_SYM   3
-#define WEN_FUNC  4
+#define WEN_BASE  1
+#define WEN_NAV   2
+#define WEN_NUM   3
+#define WEN_SYM   4
 // Acute
 #define WEN_AL    5
 // Double acute
@@ -29,21 +29,22 @@
 #define WEN_DL    7
 #define WEN_TAIPO 8
 
-#define WEN_LAYER_SET WEN_BASE WEN_NAV WEN_NUM WEN_SYM WEN_FUNC WEN_AL WEN_DAL WEN_DL
+#define WEN_LAYER_SET WEN_BASE WEN_NAV WEN_NUM WEN_SYM WEN_AL WEN_DAL WEN_DL
+
+// LAYERS FOR WIN-HUNGARIAN-QUERTZ HOST (WHU)
 
 #define WHU_BASE  9
 #define WHU_NAV   10
 #define WHU_NUM   11
 #define WHU_SYM   12
-#define WHU_FUNC  13
 // Acute
-#define WHU_AL    14
+#define WHU_AL    13
 // Double acute
-#define WHU_DAL   15
+#define WHU_DAL   14
 // Diaresis
-#define WHU_DL    16
-#define WHU_TAIPO 17
+#define WHU_DL    15
+#define WHU_TAIPO 16
 
-#define WHU_LAYER_SET WHU_BASE WHU_NAV WHU_NUM WHU_SYM WHU_FUNC WHU_AL WHU_DAL WHU_DL
+#define WHU_LAYER_SET WHU_BASE WHU_NAV WHU_NUM WHU_SYM WHU_AL WHU_DAL WHU_DL
 
 #define MHU_BASE  0
